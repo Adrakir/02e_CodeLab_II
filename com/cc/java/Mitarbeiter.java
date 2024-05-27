2a42f1ca110ac2a65ab916c446682b6d8d8294fd
Adrakir <168722517+Adrakir@users.noreply.github.com>
2024-05-27 18:46:09 +0200

    Create Mitarbeiter.java

---
+++ com/cc/java/Mitarbeiter.java
@@ -0,0 +1,29 @@
+package com.cc.java
+
+pucblic class Mitarbeiter {
+    protected String name;
+    protected String vorname;
+    protected String funktion;
+    protected int eintrittsjahr;
+
+    // Konstruktor
+    public Mitarbeiter(String name, String vorname, String funktion, int eintrittsjahr) {
+        this.name = name;
+        this.vorname = vorname;
+        this.funktion = funktion;
+        this.eintrittsjahr = eintrittsjahr;
+    }
+
+    // getInfo-Methode mit Überladung
+    public String getInfo() {
+        return "Name: " + name + ", Vorname: " + vorname;
+    }
+
+    public String getInfo(boolean details) {
+        if (details) {
+            return getInfo() + ", Funktion: " + funktion + ", Eintrittsjahr: " + eintrittsjahr;
+        } else {
+            return getInfo();
+        }
+    }
+}
