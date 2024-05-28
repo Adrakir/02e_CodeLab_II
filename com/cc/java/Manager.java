118d0ebe61e189f90c606ecfa08cb1d3b9c63360
Adrakir <168722517+Adrakir@users.noreply.github.com>
2024-05-28 09:34:00 +0200

    Update Manager.java

--- com/cc/java/Manager.java
+++ com/cc/java/Manager.java
@@ -9,8 +9,5 @@ class Manager extends Mitarbeiter {
     public String getInfo(boolean details) {
         return super.getInfo(details) + ", Abteilung: " + abteilung; }
 
-          Mitarbeiter mitarbeiter1 = new Mitarbeiter("Müller", "Max", "Entwickler", 2020);
-        Mitarbeiter mitarbeiter2 = new Mitarbeiter("Schmidt", "Anna", "Designer", 2022);
-        Manager manager = new Manager("Weber", "Lisa", "CEO", 2018, "Management");
 }
 
