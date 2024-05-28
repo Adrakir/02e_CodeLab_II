1268c679833c7a3345e2d850d8d6a2b1d141f05a
Adrakir <168722517+Adrakir@users.noreply.github.com>
2024-05-28 09:27:34 +0200

    Update App.java

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,6 +3,6 @@ package com.cc.java
 public class App {
 
 public static void main(String[] args) {
-     
-        System.out.println(mitarbeiter2.getInfo(true));
+       System.out.println(mitarbeiter2.getInfo(true,true));
+        System.out.println(mitarbeiter2.getInfo(true,true));
         System.out.println(manager.getInfo(true, true));}}
