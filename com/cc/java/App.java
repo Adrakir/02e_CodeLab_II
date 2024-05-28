7d276ebb5ed6e7f7828b0325999ac62263db99fa
Adrakir <168722517+Adrakir@users.noreply.github.com>
2024-05-28 09:33:40 +0200

    Update App.java

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,8 +1,10 @@
 package com.cc.java
-
 public class App {
-
-public static void main(String[] args) {
+       public static 
+          Mitarbeiter mitarbeiter1 = new Mitarbeiter("Müller", "Max", "Entwickler", 2020);
+        Mitarbeiter mitarbeiter2 = new Mitarbeiter("Schmidt", "Anna", "Designer", 2022);
+        Manager manager = new Manager("Weber", "Lisa", "CEO", 2018, "Management");
+}void main(String[] args) {
        System.out.println(mitarbeiter2.getInfo(true,true));
         System.out.println(mitarbeiter2.getInfo(true,true));
         System.out.println(manager.getInfo(true, true));}}
